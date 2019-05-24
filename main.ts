1fae907616a5a7db9c5aee064b9051bf17984a04
Daniel Prince <danielahprince@live.com>
2019-05-24 18:58:59 +0200

    Added more buttons and corresponding hooks.

--- main.ts
+++ main.ts
@@ -1,12 +1,23 @@
-console.log("hello world")
-
 const add = (l:number, r:number):number => l + r;
 const subtract = (l:number, r:number):number => l - r;
 const multiply = (l:number, r:number):number => l * r;
-const devide = (l:number, r:number):number => l / r;
+const devide = (l:number, r:number):number => (r != 0)? l / r : NaN;
+const operators = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": devide,
+}
 
+const disp = document.querySelector(".textarea");
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const functionButtons = document.querySelectorAll(".other");
 
-// this is how you strongly type a argument which is a function.
-function operate(operator:(l: number, r:number) => number, left:number, right:number):number {
-    return operator(left, right); 
+var operator = "";
+var left = 0;
+var right = 0;
+
+function operate(operator:string, left:number, right:number):number {
+    return operators[operator](left, right);
 }
